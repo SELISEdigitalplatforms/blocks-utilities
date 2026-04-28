f33cfd33b43cbc8681418dbe00f2ce42c1362e90
Kazi Lakit <lakitur.rahman@selisegroup.com>
2026-04-29 00:23:25 +0600

    update gitignore to exclude logs directory

---
+++ server/Cloud.LmtService/Models/Logs/LogProjection.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cloud.LmtService.Models.Logs
+{
+    [BsonIgnoreExtraElements]
+    public class LogProjection
+    {
+        public DateTime? Timestamp { get; set; }
+        public string Level { get; set; }
+        public string Message { get; set; }
+        public string TraceId { get; set; }
+        public string SpanId { get; set; }
+        public string ServiceName { get; set; } = string.Empty;
+        public string ActionName { get; set; } = string.Empty;
+    }
+}
---
+++ server/Cloud.LmtService/Repositories/Logs/ILogRepository.cs
@@ -0,0 +1,14 @@
+﻿using Cloud.LmtService.Models.Logs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cloud.LmtService.Repositories.Logs
+{
+    public interface ILogRepository
+    {
+        Task<IQueryable<LogProjection>> GetLogs(LiveLogRequest query);
+        Task<(IQueryable<LogProjection>, long)> GetLogs(GetLogsRequest query);
+        Task<(IQueryable<LogProjection>, long)> GetLogs(LogsByDateRequest request);
+    }
+}
