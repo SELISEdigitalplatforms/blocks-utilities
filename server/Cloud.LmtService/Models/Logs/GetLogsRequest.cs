03366ed56dd2e2e6f544dfe53dcf3f373d79d99c
Kazi Lakit <lakitur.rahman@selisegroup.com>
2026-04-29 00:23:25 +0600

    update gitignore to exclude logs directory

---
+++ server/Cloud.LmtService/Models/Logs/GetLogsRequest.cs
@@ -0,0 +1,36 @@
+﻿using Blocks.Genesis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cloud.LmtService.Models.Logs
+{
+    public class GetLogsRequest : BaseGetsRequest<GetLogsRequestFilter>, IProjectKey
+    {
+        public string? Search { get; set; }
+        public required string ServiceName { get; set; }
+        public string? ProjectKey { get; set; }
+    }
+
+    public class GetLogsRequestFilter
+    {
+        public DateTime? StartDate { get; set; } = null;
+        public DateTime? EndDate { get; set; } = null;
+        public string? Level { get; set; }
+        public string? TraceId { get; set; }
+        public string? SpanId { get; set; }
+    }
+
+    public class GetLogsResponse : BaseQueryListResponse<IQueryable<object>>
+    {
+
+    }
+    public class GetRestoredLogsByTraceRequest : BaseGetsRequest<object>, IProjectKey
+    {
+        public required string RequestId { get; set; }
+        public required string TraceId { get; set; }
+        public string? SpanId { get; set; }
+        public string? Level { get; set; }
+        public string? ProjectKey { get; set; }
+    }
+}
