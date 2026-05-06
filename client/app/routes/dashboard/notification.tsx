61c9fef212c95e4feec5de9d61deeb47f9087baf
Sangay Thinley <59992112+sangayt1997@users.noreply.github.com>
2026-05-07 03:08:32 +0600

    fix(blocks-utility): fixed the breadcrumb issues of navigation in utilities module

--- client/app/routes/dashboard/notification.tsx
+++ client/app/routes/dashboard/notification.tsx
@@ -1,8 +1,10 @@
 import NotificationConfigurationList from "@blocks-utilities/notification/pages/notification-list";
+import PageBreadcrumb from "@/components/breadcrumb/breadcrumb";
 
 export default function NotificationPage() {
   return (
     <div className="flex flex-col gap-6 p-6">
+      <PageBreadcrumb />
       <div className="flex w-full justify-between text-high-emphasis">
         <h3 className="text-2xl font-bold tracking-tight">Notifications</h3>
       </div>
