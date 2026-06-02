1c8a6e52b00d681f9b5f8559055e9825f4614767
Sangay Thinley <59992112+sangayt1997@users.noreply.github.com>
2026-06-02 22:26:37 +0600

    fix(blocks-utilities): fixed the env redirection issue

--- client/app/components/environment-card/environment-card.tsx
+++ client/app/components/environment-card/environment-card.tsx
@@ -31,7 +31,7 @@ export const EnvironmentCard = ({
 
   const onClickHandler = (): void => {
     setSelectedProject(project);
-    navigate("/services/authentication/users");
+    navigate("/email");
   };
 
   const handleCardClick = (): void => {
