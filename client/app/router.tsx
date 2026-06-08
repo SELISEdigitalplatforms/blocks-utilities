f16ef1734546c660b98f901a2d8fd1be40c1d403
Md. Rifath Sahriar <Anubis1080p@gmail.com>
2026-06-08 17:15:59 +0600

    fix(blocks-utilities): update LoginPage component and bump blocks-kit version to 0.0.7

--- client/app/router.tsx
+++ client/app/router.tsx
@@ -70,7 +70,7 @@ export const router = createBrowserRouter([
               </PublicGuard>
             ),
             children: [
-              { path: "/login", element: <LoginSimplePage /> },
+              { path: "/login", element: <LoginPage name="blocks-utilities" /> },
               {
                 path: "/dashboard/callback",
                 element: <CallbackPage redirectUrl="/dashboard" />,
