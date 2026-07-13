5cd6eb12dc62e8fc15e4ef4527267717853fae43
SaimonSelise <saimon.siddique@selisegroup.com>
2026-07-13 14:08:17 +0600

    refactor: replace local ThemeProvider with blocks-kit provider and clean up unused theme code

--- client/app/main.tsx
+++ client/app/main.tsx
@@ -5,7 +5,7 @@ import { NuqsAdapter } from "nuqs/adapters/react-router/v6";
 import { Toaster } from "./components/ui-kits/toaster/toaster";
 import QueryProvider from "./providers/query-provider";
 import { router } from "./router";
-import { ThemeProvider } from "./hooks/use-theme";
+import { ThemeProvider } from "@seliseblocks/blocks-kit/providers";
 import { BlocksAppLayout } from "@seliseblocks/blocks-kit";
 import { TooltipProvider } from "./components/ui-kits/tooltip/tooltip";
 import "./styles/globals.css";
