30d6a8d57cf42540732c673dca0814e8112e86b9
fahad-selise <mohammad.fahad@selisegroup.com>
2026-07-23 03:24:30 +0600

    fix(payment): tolerate unavailable vault secrets at startup

--- server/Api/Program.cs
+++ server/Api/Program.cs
@@ -21,10 +21,10 @@ var secret =
         .ConfigureLogAndSecretsAsync(
             serviceName,
             vaultType);
-//var paymentVault = Vault.GetCloudVault(vaultType);
-//var providerTokenEncryptionKeyRing =
-//    await ProviderTokenEncryptionKeyRingVaultLoader
-//        .LoadAsync(paymentVault);
+var paymentVault = Vault.GetCloudVault(vaultType);
+var providerTokenEncryptionKeyRingLoadResult =
+    await ProviderTokenEncryptionKeyRingVaultLoader
+        .LoadSafelyAsync(paymentVault);
 var builder = WebApplication.CreateBuilder(args);
 
 ApplicationConfigurations.ConfigureApiEnv(builder, args);
@@ -75,9 +75,11 @@ ApplyFrontendRuntimeSettings(builder.Configuration, wwwrootPath);
 //services.AddCloudLmtServices();
 //services.AddCloudConfigurationServices();
 services.RegisterAllMailApplicationServices();
-//services.AddSingleton<IVault>(_ => paymentVault);
-//services.AddSingleton<IProviderTokenEncryptionKeyRing>(
-//    _ => providerTokenEncryptionKeyRing);
+services.AddSingleton<IVault>(_ => paymentVault);
+services.AddSingleton<IProviderTokenEncryptionKeyRing>(
+    _ => providerTokenEncryptionKeyRingLoadResult.KeyRing);
+services.AddSingleton(
+    providerTokenEncryptionKeyRingLoadResult.Readiness);
 services.RegisterPaymentDomainServices(builder.Configuration);
 services.RegisterAllNotificationApplicationServices();
 services.RegisterUtilityServices();
