5f4288a9d1f4ba23fcdd82af5fe980e5e58877f8
rezwanx <rezwanxx@gmail.com>
2026-07-24 06:29:29 +0200

    chore: add Prettier config and wire eslint-config-prettier
    Add identical .prettierrc/.prettierignore to client and e2e, add format and
    format:check scripts, and place "prettier" last in the ESLint extends so the
    two tools do not conflict.

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -23,6 +23,7 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
+    "prettier",
   ],
   rules: {
     "react/react-in-jsx-scope": "off",
