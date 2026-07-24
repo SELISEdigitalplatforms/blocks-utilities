54900921697172bf93550e4170a55500a48c84ea
rezwanx <rezwanxx@gmail.com>
2026-07-24 18:20:36 +0200

    test: cover PaymentResponseMapper, ProviderFailureReasonMapper branches and MessageConfigurationHelper

---
+++ server/XUnitTest/Messaging/MessageConfigurationHelperTests.cs
@@ -0,0 +1,42 @@
+using FluentAssertions;
+using Utility.DomainService.Messaging;
+
+namespace XUnitTest.Messaging;
+
+public sealed class MessageConfigurationHelperTests
+{
+    [Theory]
+    [InlineData("amqp://<username>:<password>@localhost:5672")]
+    [InlineData("amqps://<username>:<password>@broker:5671")]
+    public void Amqp_connection_strings_select_rabbitmq(string connectionString)
+    {
+        var configuration = MessageConfigurationHelper.GetMessageConfiguration(
+            connectionString,
+            "queue-a",
+            "queue-b");
+
+        configuration.RabbitMqConfiguration.Should().NotBeNull();
+        configuration.AzureServiceBusConfiguration.Should().BeNull();
+        configuration.RabbitMqConfiguration!.ConsumerSubscriptions
+            .Should().HaveCount(2);
+    }
+
+    [Theory]
+    [InlineData("Endpoint=sb://example.servicebus.windows.net/;SharedAccessKeyName=x;SharedAccessKey=y")]
+    [InlineData("not-a-uri")]
+    public void Non_amqp_connection_strings_select_azure_service_bus(string connectionString)
+    {
+        var configuration = MessageConfigurationHelper.GetMessageConfiguration(
+            connectionString,
+            "queue-a",
+            "queue-b",
+            "queue-c");
+
+        configuration.AzureServiceBusConfiguration.Should().NotBeNull();
+        configuration.RabbitMqConfiguration.Should().BeNull();
+        configuration.AzureServiceBusConfiguration!.Queues
+            .Should().BeEquivalentTo("queue-a", "queue-b", "queue-c");
+        configuration.AzureServiceBusConfiguration.QueueMaxDeliveryCount
+            .Should().Be(10);
+    }
+}
